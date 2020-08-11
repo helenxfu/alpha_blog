c2023d1a8bc8c5a295d94cac5a807f1cc9a902e6
helenxfu <helenxfu@gmail.com>
2020-08-11 21:53:28 +0900

    linked users and articles table

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
   root "pages#home"
   get "about", to: "pages#about"
   resources :articles
+  resources :users, except: [:new]
+  get "signup", to: "users#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
